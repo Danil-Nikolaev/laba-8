30d64eaa7bb967e2b740c77d1b9fecce23e9d8eb
Danil-Nikolaev <danil_nikolaev_96@bk.ru>
2022-11-23 19:08:36 +0300

    initial commit

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -26,14 +26,17 @@ class CalcController < ApplicationController
   def view
     v1 = params[:v1]
     v2 = params[:v2]
-    if numeric(v1) && numeric(v2)
-      v1 = v1.to_i
-      v2 = v2.to_i
-      @result_one = evklid(v1, v2)
-      @result_two = (v1 * v2) / @result_one
-    else
+    digit = true
+    if /^\d$/.match(v1).nil? || /^\d$/.match(v2).nil?
       @result_one = 'Unknow'
       @result_two = 'Unknow'
+      digit = false
     end
+    return unless digit
+
+    v1 = v1.to_i
+    v2 = v2.to_i
+    @result_one = evklid(v1, v2)
+    @result_two = (v1 * v2) / @result_one
   end
 end
